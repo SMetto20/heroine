2fb8ddc0685b5ed8a7251f50d3c542c44fb719a3
SMetto20 <smetto20@gmail.com>
2022-05-03 16:05:55 +0300

    ft-adds form path

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import static spark.Spark.*;
 
 import java.util.*;
 
+import org.example.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -20,5 +21,26 @@ public class App {
             return new ModelAndView(new HashMap(), "home.hbs");
 
         }, new HandlebarsTemplateEngine());
+        get("/form", (request, response) -> {
+            name = request.queryParams("name");
+            power = request.queryParams("power");
+            weakness = request.queryParams("weakness");
+            age = request.queryParams("age");
+            height = request.queryParams("height");
+
+
+
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList mySquadArrayList = Squad.getAll();
+            model.put("mySquad", mySquadArrayList);
+            model.put("name",name);
+            model.put("power",power);
+            model.put("weakness",weakness);
+            model.put("age",age);
+            model.put("height",height);
+            model.put("squadList",squadList);
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
