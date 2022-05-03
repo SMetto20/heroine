ba97e1ba750acb72eca141c6aab1fc16522667e5
SMetto20 <smetto20@gmail.com>
2022-05-03 16:00:34 +0300

    adds squad properties

--- src/main/java/org/example/Squad.java
+++ src/main/java/org/example/Squad.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Squad {
+    private static ArrayList<Squad> instances = new ArrayList<>();
+    private String mName;
+    private int mSize;
+    private String mCause;
+    public String mGender;
 }
