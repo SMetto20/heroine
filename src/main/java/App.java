9e6d51648fa64a2eacfd8bedb67893eefa254cd9
SMetto20 <smetto20@gmail.com>
2022-05-03 15:29:55 +0300

    ft-adds path to home.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,24 @@
+import static spark.Spark.*;
+
+import java.util.*;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 
 
 
 
 public class App {
+    public static void main(String[] args) {
+//
+
+
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+
+            return new ModelAndView(new HashMap(), "home.hbs");
+
+        }, new HandlebarsTemplateEngine());
+    }
 }
