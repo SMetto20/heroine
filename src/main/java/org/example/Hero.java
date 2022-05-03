609399812cee7e28aca772ef1317bb90ab7d1424
SMetto20 <smetto20@gmail.com>
2022-05-03 15:55:57 +0300

    ft-adds hero constructor

--- src/main/java/org/example/Hero.java
+++ src/main/java/org/example/Hero.java
@@ -6,4 +6,12 @@ public class Hero {
     private String power;
     private int age;
     private String weakness;
+    public Hero(String name, int height, String power, int age,String weakness){
+        this.name = name;
+        this.height= height;
+        this.power= power;
+        this.age = age;
+        this.weakness=weakness;
+    }
+
 }
